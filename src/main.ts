9845eaa14559bfbb70e81ddec6fc3e84e170df4c
Térence <terence.hollander@decathlon.com>
2022-03-19 13:59:12 +0100

    chore: add prettier

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,12 @@ import * as core from '@actions/core';
 
 async function run() {
   try {
-    const message: string = core.getInput("message");
-    const github_token: string = core.getInput("GITHUB_TOKEN");
+    const message: string = core.getInput('message');
+    const github_token: string = core.getInput('GITHUB_TOKEN');
 
     const context = github.context;
     if (context.payload.pull_request == null) {
-      core.setFailed("No pull request found.");
+      core.setFailed('No pull request found.');
       return;
     }
     const pull_request_number = context.payload.pull_request.number;
