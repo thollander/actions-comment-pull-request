288dcd6311950ecc16d76bfb8d8fb1ec46aec8a1
Térence <terence.hollander@decathlon.com>
2022-03-19 14:15:45 +0100

    feat: ability to specify pr_number for manual action
    Co-authored-by: mvarrieur

--- src/main.ts
+++ src/main.ts
@@ -5,18 +5,21 @@ async function run() {
   try {
     const message: string = core.getInput('message');
     const github_token: string = core.getInput('GITHUB_TOKEN');
+    const pr_number: string = core.getInput('pr_number');
 
     const context = github.context;
-    if (context.payload.pull_request == null) {
-      core.setFailed('No pull request found.');
-      return;
-    }
-    const pull_request_number = context.payload.pull_request.number;
+    const pull_number = parseInt(pr_number) || context.payload.pull_request?.number;
 
     const octokit = github.getOctokit(github_token);
-    const new_comment = await octokit.rest.issues.createComment({
+
+    if (!pull_number) {
+      core.setFailed('No pull request in input neither in current context.');
+      return;
+    }
+
+    await octokit.rest.issues.createComment({
       ...context.repo,
-      issue_number: pull_request_number,
+      issue_number: pull_number,
       body: message,
     });
   } catch (error) {
